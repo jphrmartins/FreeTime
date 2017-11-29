17ae6335fb616be6dbfa7060a3bd243a6cdca183
Jp Martins <jphmartins@gmail.com>
2017-11-29 15:05:23 -0200

    Angular Prototipo APS

--- amigoSecretoPrototipo/app.js
+++ amigoSecretoPrototipo/app.js
@@ -14,18 +14,17 @@
             $scope.amigo = {'id': index + 1, 'nome': ''};
         }
         $scope.adicionar = function(){
-            for (let index = 0; index < 2; index++) {
                 var id = $scope.listaAmigos.length + 1;
                 $scope.amigo.id = id;
                 $scope.listaAmigos.push($scope.amigo);
                 $scope.amigo = {'id': 0, 'nome': ''}
-            }
         }
         $scope.sortear = function(){
             var listaSorteioPai = removeNomeInvalido();
-            if(listaSorteioPai.length%2 === 0){
-                var listaFilha = [];
-                var volta = 0;
+            var listaFilha = [];
+            var volta = 0; 
+            if(listaSorteioPai.length !==1){
+                debugger;
                 while(volta < listaSorteioPai.length){
                     var id = Math.floor((Math.random()*listaSorteioPai.length)+1);
                     var index = pegarPosicaoListaPai(id, listaSorteioPai);
@@ -34,16 +33,33 @@
                             volta = amigoSecretoSelecionado(volta, index, listaFilha, listaSorteioPai);
                         }
                     } else {
-                        if(id !== listaSorteioPai[volta].id){      
-                            if(casoPessoaJaRetirada(listaFilha, id)){
-                                volta = amigoSecretoSelecionado(volta, index, listaFilha, listaSorteioPai);
+                        if (pessoaQueFaltaUltimaDaLista(volta, listaSorteioPai, listaFilha, id)){
+                            alert('somente: ' + listaSorteioPai[volta].nome + ', Pode ver a proxima tela.')                            
+                            alert(listaSorteioPai[volta].nome + ', Você retirou: ' 
+                                + listaSorteioPai[index+1].nome + '. Por favor, avance para a proxima tela');
+                            
+                                alert('somente: ' + listaSorteioPai[volta+1].nome + ', Pode ver a proxima tela.')                            
+                            alert(listaSorteioPai[volta+1].nome + ', Você retirou: ' 
+                                + listaSorteioPai[index].nome + '. Por favor, avance para a proxima tela');
+                            break;
+                        } else if(idPessoaDaVolta(id, listaSorteioPai, volta)){      
+                            if(casoPessoaAindaNaoRetirada(listaFilha, id)){
+                            volta = amigoSecretoSelecionado(volta, index, listaFilha, listaSorteioPai);
                             }
-                        }
-                    }   
+                        }   
+                    }
                 }
-            } else {
-                alert("Número de pessoas invalidas. Requer um número par de pessoas");
+            }else{
+                alert('Número de pessoas invalidas');
             }
+        }
+        function idPessoaDaVolta(id, listaSorteioPai, volta) {
+            return id !== listaSorteioPai[volta].id;
+        }
+        function pessoaQueFaltaUltimaDaLista(volta, listaSorteioPai,listaFilha, id) {
+            debugger;
+            return volta + 2 === listaSorteioPai.length  && id !== listaSorteioPai[listaSorteioPai.length-1].id
+                    && casoPessoaAindaNaoRetirada(listaFilha, listaSorteioPai[listaSorteioPai.length-1].id) 
         }
         function casoPessoaAindaNaoRetirada(listaFilha, id) {
             for (let loop = 0; loop < listaFilha.length; loop++) {
@@ -62,6 +78,7 @@
             return volta;
         }
         function pegarPosicaoListaPai(id, lista) {
+            debugger;
             var index = 0;
             while (index < lista.length) {
                 if(lista[index].id === id){
